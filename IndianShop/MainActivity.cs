1424b902fbbf4298807fbc131c13382d5f01e4d1
tejurajurkar <tejurajurkar@gmail.com>
2020-07-10 23:00:47 +0530

    first commit

---
+++ IndianShop/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace IndianShop
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+    
+            protected override void OnCreate(Bundle savedInstanceState)
+            {
+                base.OnCreate(savedInstanceState);
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.activity_main);
+                var gridview = FindViewById<GridView>(Resource.Id.gridview);
+                gridview.Adapter = new ImageAdapter(this);
+
+                gridview.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args) {
+                    Toast.MakeText(this, args.Position.ToString(), ToastLength.Short).Show();
+                };
+
+
+
+
+            }
+        }
+
+
+
+    
+
+}
